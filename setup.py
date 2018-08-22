2cf42a7fff562ce6f12d1ff76c24c39d7dd29f79
awsteiner <awsteiner@mykolab.com>
2018-08-22 13:58:55 -0600

    Improved setup.py properties.

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
-from setuptools import setup
+import setuptools
 
-setup(name='o2sclpy',version='0.922',
-      description='Python extensions for O2scl',
-      url='https://isospin.roam.utk.edu/static/code/o2sclpy',
-      author='Andrew W. Steiner',
-      author_email='awsteiner@mykolab.com',license='GPLv3',
-      packages=['o2sclpy'],install_requires=['h5py','numpy','matplotlib'],
-      zip_safe=False,scripts=['bin/o2graph'],
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-          'Programming Language :: Python :: 3 :: Only',
-          'Topic :: Scientific/Engineering'
-      ])
+with open("README.md","r") as file_handle:
+    long_description=file_handle.read()
+
+setuptools.setup(name='o2sclpy',
+                 version='0.922',
+                 author='Andrew W. Steiner',
+                 author_email='awsteiner@mykolab.com',
+                 description='Python extensions for O2scl',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",                 
+                 url='https://isospin.roam.utk.edu/static/code/o2sclpy',
+                 license='GPLv3',
+                 packages=['o2sclpy'],
+                 install_requires=['h5py','numpy','matplotlib'],
+                 zip_safe=False,scripts=['bin/o2graph'],
+                 classifiers=[
+                     'Development Status :: 4 - Beta',
+                     ('License :: OSI Approved :: '+
+                      'GNU General Public License v3 (GPLv3)'),
+                     'Programming Language :: Python :: 3 :: Only',
+                     'Topic :: Scientific/Engineering'
+                 ])
