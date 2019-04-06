b9e83d34c1ae5f28b185c6d4ef80055c4df6eb43
awsteiner <awsteiner@mykolab.com>
2019-04-05 21:31:14 -0400

    Created slack_notify().

--- o2sclpy/__init__.py
+++ o2sclpy/__init__.py
@@ -54,6 +54,11 @@ import platform
 # For code.interact() in 'python' command
 import code 
 
+# For slack notify
+import json
+import requests
+import socket
+
 """
 The version number string
 """
@@ -417,6 +422,63 @@ param_list=[
     ["zset","If true, z-axis limits have been set (default False)."]
 ]
 
+def slack_notify(webhook_url,channel_name,title,
+                 message,subtitle='',emoji=':computer:'):
+    """
+    https://www.amphioxus.org/content/slack-notifications-scripts#python
+    https://www.webpagefx.com/tools/emoji-cheat-sheet/
+    JSON format found in https://github.com/sulhome/bash-slack
+    """
+
+    # Formulate message
+    if subtitle!='':
+        msgdata={
+            "channel": channel_name,
+            "username": socket.gethostname(),
+            "icon_emoji": emoji,
+            "attachments": [
+                {
+                    "fallback": title,
+                    "color": "good",
+                    "title": title,
+                    "fields": [{
+                        "title": subtitle,
+                        "value": message,
+                        "short": False
+                    }]
+                }
+            ]
+        }
+    else:
+        msgdata={
+            "channel": channel_name,
+            "username": socket.gethostname(),
+            "icon_emoji": emoji,
+            "attachments": [
+                {
+                    "fallback": title,
+                    "color": "good",
+                    "title": title,
+                    "fields": [{
+                        "value": message,
+                        "short": False
+                    }]
+                }
+            ]
+        }
+    
+    # Contact slack
+    response=requests.post(webhook_url,data=json.dumps(msgdata),
+                           headers={'Content-Type': 'application/json'}
+    )
+
+    # If error is returned
+    if response.status_code!=200:
+        str_temp=('Request to Slack returned '+
+                  'error:\n{}. Response is:\n{}')
+        raise ValueError(str_temp.format(response.status_code,
+                                         response.text))
+    
 def force_bytes(obj):
     """
     This function returns the bytes object corresponding to ``obj``
