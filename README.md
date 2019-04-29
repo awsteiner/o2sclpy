376c8ff47c8f9c3d89f031104131d7706c07fcc9
Andrew W. Steiner <awsteiner@mykolab.com>
2019-04-29 12:40:29 -0400

    This was the 0.923 release, but the webpage URL was out-dated, thus it will probably be the 0.923.1 release...

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # o2sclpy
 Python extensions for O2scl
 
-Documentation is available at https://isospin.roam.utk.edu/static/code/o2sclpy .
+Documentation is available at https://neutronstars.utk.edu/code/o2sclpy .
--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md","r") as file_handle:
     long_description=file_handle.read()
 
 setuptools.setup(name='o2sclpy',
-                 version='0.922.1',
+                 version='0.923.1',
                  author='Andrew W. Steiner',
                  author_email='awsteiner@mykolab.com',
                  description='Python extensions for O2scl',
                  long_description=long_description,
                  long_description_content_type="text/markdown",                 
-                 url='https://isospin.roam.utk.edu/static/code/o2sclpy',
+                 url='https://neutronstars.utk.edu/code/o2sclpy',
                  license='GPLv3',
                  packages=['o2sclpy'],
                  install_requires=['h5py','numpy','matplotlib'],
