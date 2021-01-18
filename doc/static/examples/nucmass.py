ba8712833157765a90d9b17f3399f40b283950a8
Andrew W. Steiner <awsteiner@mykolab.com>
2021-01-17 20:35:55 -0800

    Updated nucmass example.

--- doc/static/examples/nucmass.py
+++ doc/static/examples/nucmass.py
@@ -4,7 +4,16 @@ l=o2sclpy.linker()
 l.get_library_settings()
 l.link_o2scl_o2graph(True)
 
+# Instantiate and load the Atomic Mass Evaluation
 ame=o2sclpy.nucmass_ame(l.o2scl_part)
-
 o2sclpy.ame_load(l.o2scl_part,ame,'16',False)
 
+# Print out the number of entries
+print(ame.get_nentries())
+
+# Get lead-208
+nuc=o2sclpy.nucleus(l.o2scl_part)
+ame.get_nucleus(82,126,nuc)
+
+# Output the binding energy per nucleon in MeV
+print(nuc.be/208*197.33)
