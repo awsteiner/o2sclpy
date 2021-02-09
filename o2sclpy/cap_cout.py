43b12081dc1cf99c9375384629004d495c6a003d
Andrew W. Steiner <awsteiner@mykolab.com>
2021-02-09 08:30:53 -0800

    Updating cap_cout.

--- o2sclpy/cap_cout.py
+++ o2sclpy/cap_cout.py
@@ -1,21 +1,51 @@
-# from https://stackoverflow.com/questions/24277488/in-python-how-to-capture-
-# the-stdout-from-a-c-shared-library-to-a-variable
-import ctypes
+#  -------------------------------------------------------------------
+#  
+#  Copyright (C) 2006-2020, Andrew W. Steiner
+#  
+#  This file is part of O2sclpy.
+#  
+#  O2sclpy is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#  
+#  O2sclpy is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with O2sclpy. If not, see <http://www.gnu.org/licenses/>.
+#  
+#  -------------------------------------------------------------------
+#
 import os
-import sys
 import threading
 
-# Create pipe and dup2() the write end of it on top of stdout, saving a copy
-# of the old stdout
-#stdout_fileno = sys.stdout.fileno()
-
 class cap_cout:
+    """
 
+    Capture std::cout into a python string in order to 
+    output it to a jupyter notebook
+
+    Based on 
+
+    https://stackoverflow.com/questions/24277488/in-python-how-to-
+    capture-the-stdout-from-a-c-shared-library-to-a-variable
+
+    and see also 
+
+    https://dzone.com/articles/redirecting-all-kinds-stdout
+
+    and 
+
+    """
+    
     stdout_fileno=1
-    captured_stdout = ''
+    captured_stdout=''
     stdout_pipe=0
     stdout_save=0
-    t=0
+    thr=0
     
     def drain_pipe(self):
         while True:
@@ -25,17 +55,17 @@ class cap_cout:
             self.captured_stdout += str(data,'UTF-8')
         return
         
-    def __init__(self):
+    def open(self):
         self.stdout_save=os.dup(self.stdout_fileno)
         self.stdout_pipe=os.pipe()
         os.dup2(self.stdout_pipe[1],self.stdout_fileno)
         os.close(self.stdout_pipe[1])
-        self.t=threading.Thread(target=self.drain_pipe)
-        self.t.start()
+        self.thr=threading.Thread(target=self.drain_pipe)
+        self.thr.start()
 
     def close(self):
         os.close(self.stdout_fileno)
-        self.t.join()
+        self.thr.join()
 
         # Clean up the pipe and restore the original stdout
         os.close(self.stdout_pipe[0])
