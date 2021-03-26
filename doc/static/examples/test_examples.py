8614fcb9d1794bf6275977f6b540b7b29d59552f
Andrew W. Steiner <awsteiner@mykolab.com>
2021-03-26 16:44:12 -0400

    Adding chmod 755 to test.scr in test_examples.py.

--- doc/static/examples/test_examples.py
+++ doc/static/examples/test_examples.py
@@ -32,78 +32,91 @@ def test_colors_near():
 
 def test_markers():
     remove_show('doc/static/examples/markers.scr')
+    os.system("chmod 755 temp.scr')
     os.system('cd doc/static/examples; ./temp.scr')
     compare_images('markers')
     return
 
 def test_modax():
     remove_show('doc/static/examples/modax.scr')
+    os.system("chmod 755 temp.scr')
     os.system('cd doc/static/examples; ./temp.scr')
     compare_images('modax')
     return
 
 def test_subplots1():
     remove_show('doc/static/examples/subplots1.scr')
+    os.system("chmod 755 temp.scr')
     os.system('cd doc/static/examples; ./temp.scr')
     compare_images('subplots1')
     return
 
 def test_subplots2():
     remove_show('doc/static/examples/subplots2.scr')
+    os.system("chmod 755 temp.scr')
     os.system('cd doc/static/examples; ./temp.scr')
     compare_images('subplots2')
     return
 
 def test_subplots3():
     remove_show('doc/static/examples/subplots3.scr')
+    os.system("chmod 755 temp.scr')
     os.system('cd doc/static/examples; ./temp.scr')
     compare_images('subplots3')
     return
 
 def test_table_plot():
     remove_show('doc/static/examples/table_plot.scr')
+    os.system("chmod 755 temp.scr')
     os.system('cd doc/static/examples; ./temp.scr')
     compare_images('table_plot')
     return
 
 def test_table_plotv():
     remove_show('doc/static/examples/table_plotv.scr')
+    os.system("chmod 755 temp.scr')
     os.system('cd doc/static/examples; ./temp.scr')
     compare_images('table_plotv')
     return
 
 def test_table_rplot():
     remove_show('doc/static/examples/table_rplot.scr')
+    os.system("chmod 755 temp.scr')
     os.system('cd doc/static/examples; ./temp.scr')
     compare_images('table_rplot')
     return
 
 def test_table_errorbar():
     remove_show('doc/static/examples/table_errorbar.scr')
+    os.system("chmod 755 temp.scr')
     os.system('cd doc/static/examples; ./temp.scr')
     compare_images('table_errorbar')
     return
 
 def test_textbox():
     remove_show('doc/static/examples/textbox.scr')
+    os.system("chmod 755 temp.scr')
     os.system('cd doc/static/examples; ./temp.scr')
     compare_images('textbox')
     return
 
 def test_table_scatter():
     remove_show('doc/static/examples/table_scatter.scr')
+    os.system("chmod 755 temp.scr')
     os.system('cd doc/static/examples; ./temp.scr')
     compare_images('table_scatter')
     return
 
 def test_table3d_den_plot():
     remove_show('doc/static/examples/table3d_den_plot.scr')
+    os.system("chmod 755 temp.scr')
     os.system('cd doc/static/examples; ./temp.scr')
     compare_images('table3d_den_plot')
     return
 
 def test_yt_scatter():
     remove_show('doc/static/examples/yt_scatter.scr')
+    os.system("chmod 755 temp.scr')
     os.system('cd doc/static/examples; ./temp.scr')
     compare_images('yt_scatter')
     return
