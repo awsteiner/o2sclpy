53936a43a5dd2b8c9f0736f1021d7b884c998b14
Andrew W. Steiner <awsteiner@mykolab.com>
2022-05-09 18:17:51 -0400

    created buchdahl.py.

---
+++ doc/static/examples/buchdahl.py
@@ -0,0 +1,57 @@
+# # Skyrme equation of state example for O$_2$sclpy
+
+# See the O$_2$sclpy documentation at https://neutronstars.utk.edu/code/o2sclpy for more information.
+
+# +
+import o2sclpy
+import matplotlib.pyplot as plot
+import ctypes
+import numpy
+import sys
+
+plots=True
+if 'pytest' in sys.modules:
+    plots=False
+# -
+
+# Link the O$_2$scl library:
+
+link=o2sclpy.linker()
+link.link_o2scl()
+
+# Get a copy (a pointer to) the O$_2$scl unit conversion object:
+
+cu=link.o2scl_settings.get_convert_units()
+
+# Create the Buchdahl EOS object
+
+b=o2sclpy.eos_tov_buchdahl(link)
+
+# Create the TOV solve object, set the EOS and compute the M-R curve
+
+ts=o2sclpy.tov_solve(link)
+ts.set_eos(b);
+ts.fixed(1.4,1.0e-4)
+print('Exact radius is %7.6e, computed radius is %7.6e.' %
+      (b.rad_from_gm(1.4),ts.rad))
+print('Relative difference %7.6e.' %
+      (abs(b.rad_from_gm(1.4)-ts.rad)/ts.rad))
+
+# Get the table for the TOV results
+
+tov_table=ts.get_results()
+
+if plots:
+    pl=o2sclpy.plotter()
+    pl.canvas()
+    plot.plot(tov_table['r'][0:tov_table.get_nlines()],
+              tov_table['gm'][0:tov_table.get_nlines()])
+    pl.xtitle('radius (km)')
+    pl.ytitle('gravitational mass (Msun)')
+    plot.show()
+    
+# For testing using ``pytest``:
+
+def test_fun():
+    assert numpy.allclose(Lambda,297.0,rtol=8.0e-3)
+    return
--- o2sclpy/eos.py
+++ o2sclpy/eos.py
@@ -2492,6 +2492,18 @@ class eos_tov:
         ret=func(self._ptr)
         return ret
 
+    def ed_from_pr(self,pr):
+        """
+        | Parameters:
+        | *pr*: ``double``
+        | Returns: a Python float
+        """
+        func=self._link.o2scl.o2scl_eos_tov_ed_from_pr
+        func.restype=ctypes.c_double
+        func.argtypes=[ctypes.c_void_p,ctypes.c_double]
+        ret=func(self._ptr,pr)
+        return ret
+
 
 class eos_tov_buchdahl(eos_tov):
     """
@@ -2563,6 +2575,75 @@ class eos_tov_buchdahl(eos_tov):
         func.argtypes=[ctypes.c_void_p,ctypes.c_double]
         func(self._ptr,value)
         return
+
+    @property
+    def G_km_Msun(self):
+        """
+        Property of type ``ctypes.c_double``
+        """
+        func=self._link.o2scl.o2scl_eos_tov_buchdahl_get_G_km_Msun
+        func.restype=ctypes.c_double
+        func.argtypes=[ctypes.c_void_p]
+        return func(self._ptr)
+
+    @G_km_Msun.setter
+    def G_km_Msun(self,value):
+        """
+        Setter function for eos_tov_buchdahl::G_km_Msun .
+        """
+        func=self._link.o2scl.o2scl_eos_tov_buchdahl_set_G_km_Msun
+        func.argtypes=[ctypes.c_void_p,ctypes.c_double]
+        func(self._ptr,value)
+        return
+
+    def set_baryon_density(self,nb,ed):
+        """
+        | Parameters:
+        | *nb*: ``double``
+        | *ed*: ``double``
+        """
+        func=self._link.o2scl.o2scl_eos_tov_buchdahl_set_baryon_density
+        func.argtypes=[ctypes.c_void_p,ctypes.c_double,ctypes.c_double]
+        func(self._ptr,nb,ed)
+        return
+
+    def rad_from_gm(self,gm):
+        """
+        | Parameters:
+        | *gm*: ``double``
+        | Returns: a Python float
+        """
+        func=self._link.o2scl.o2scl_eos_tov_buchdahl_rad_from_gm
+        func.restype=ctypes.c_double
+        func.argtypes=[ctypes.c_void_p,ctypes.c_double]
+        ret=func(self._ptr,gm)
+        return ret
+
+    def ed_from_r_gm(self,r,beta):
+        """
+        | Parameters:
+        | *r*: ``double``
+        | *beta*: ``double``
+        | Returns: a Python float
+        """
+        func=self._link.o2scl.o2scl_eos_tov_buchdahl_ed_from_r_gm
+        func.restype=ctypes.c_double
+        func.argtypes=[ctypes.c_void_p,ctypes.c_double,ctypes.c_double]
+        ret=func(self._ptr,r,beta)
+        return ret
+
+    def pr_from_r_gm(self,r,beta):
+        """
+        | Parameters:
+        | *r*: ``double``
+        | *beta*: ``double``
+        | Returns: a Python float
+        """
+        func=self._link.o2scl.o2scl_eos_tov_buchdahl_pr_from_r_gm
+        func.restype=ctypes.c_double
+        func.argtypes=[ctypes.c_void_p,ctypes.c_double,ctypes.c_double]
+        ret=func(self._ptr,r,beta)
+        return ret
 
 
 class eos_tov_polytrope(eos_tov):
