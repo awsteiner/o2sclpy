7bbe112acf9909c648d86906a093b48d03a206de
Andrew W. Steiner <awsteiner@mykolab.com>
2023-05-20 12:48:24 -0400

    Updating gmm_sklearn and bgmm_sklearn.

--- o2sclpy/test/test_gmm.py
+++ o2sclpy/test/test_gmm.py
@@ -23,7 +23,15 @@ import o2sclpy
 import numpy
 
 def test_gmm():
+    """
+    Test the gmm_sklearn class
+    """
 
+    print('──────────────────────────────────'
+          '─────────────────────────────────')
+    print('Testing gmm_sklearn\n')
+    
+    # Create test data, a bimodal distribution with two clear peaks
     N=100
     x=numpy.zeros((N,2))
     for i in range(0,100):
@@ -33,21 +41,66 @@ def test_gmm():
         else:
             x[i,0]=0.1+0.2*numpy.sin(i*1.0e6)
             x[i,1]=0.1+0.2*numpy.cos(i*1.0e6)
-    
+
+    # Perform the initial fit
     gs=o2sclpy.gmm_sklearn()
-    gs.set_data_str(x,'verbose=2,n_components=2')
+    gs.set_data_str(x,'verbose=1,n_components=2')
+    print(' ')
+
+    # Test components()
+    print('components():')
     print(gs.components([0.7,0.7]))
     print(gs.components([0.0,0.0]))
-    print('w',gs.gm.weights_)
-    print('m',gs.gm.means_)
-    print('c',gs.gm.covariances_)
-    print('p',gs.gm.precisions_)
-    print(gs.get_data())
+    print(gs.components([[0.7,0.7],
+                         [0.0,0.0]]))
+    print(' ')
+
+    # Test predict()
+    print('predict():')
+    print(gs.predict([0.5,0.5]))
+    print(gs.predict([0.2,0.2]))
+    print(gs.predict([[0.2,0.2],
+                      [0.1,0.1]]))
+    print(' ')
+    
+    # Test score()
+    print('score():')
+    print(gs.score([0.5,0.5]))
+    print(gs.score([0.2,0.2]))
+    print(gs.score([[0.2,0.2],
+                      [0.1,0.1]]))
+    print(' ')
+
+    # Test score_samples()
+    print('score_samples():')
+    print(gs.score_samples([0.5,0.5]))
+    print(gs.score_samples([0.2,0.2]))
+    print(gs.score_samples([[0.2,0.2],
+                            [0.1,0.1]]))
+    print(' ')
+
+    # Test sample()
+    print('sample():')
+    print(gs.sample())
+    print(gs.sample(n_samples=3))
+    print(' ')
+
+    print('Internal data:')
+    print('weights:',gs.gm.weights_)
+    print('means:',gs.gm.means_)
+    print('covariances:',gs.gm.covariances_)
+    print('precisions:',gs.gm.precisions_)
+    print(' ')
 
     return
 
 def test_bgmm():
 
+    print('──────────────────────────────────'
+          '─────────────────────────────────')
+    print('Testing bgmm_sklearn\n')
+
+    # Create test data, a bimodal distribution with two clear peaks
     N=100
     x=numpy.zeros((N,2))
     for i in range(0,100):
@@ -57,16 +110,48 @@ def test_bgmm():
         else:
             x[i,0]=0.1+0.2*numpy.sin(i*1.0e6)
             x[i,1]=0.1+0.2*numpy.cos(i*1.0e6)
-    
+
+    # Perform the initial fit
     gs=o2sclpy.bgmm_sklearn()
-    gs.set_data_str(x,'verbose=2,n_components=2')
-    print(gs.components([0.7,0.7]))
-    print(gs.components([0.0,0.0]))
-    print('w',gs.bgm.weights_)
-    print('m',gs.bgm.means_)
-    print('c',gs.bgm.covariances_)
-    print('p',gs.bgm.precisions_)
-    print(gs.get_data())
+    gs.set_data_str(x,'verbose=1,n_components=2')
+    print(' ')
+    
+    # Test predict()
+    print('predict():')
+    print(gs.predict([0.5,0.5]))
+    print(gs.predict([0.2,0.2]))
+    print(gs.predict([[0.2,0.2],
+                      [0.1,0.1]]))
+    print(' ')
+    
+    # Test score()
+    print('score():')
+    print(gs.score([0.5,0.5]))
+    print(gs.score([0.2,0.2]))
+    print(gs.score([[0.2,0.2],
+                      [0.1,0.1]]))
+    print(' ')
+
+    # Test score_samples()
+    print('score_samples():')
+    print(gs.score_samples([0.5,0.5]))
+    print(gs.score_samples([0.2,0.2]))
+    print(gs.score_samples([[0.2,0.2],
+                            [0.1,0.1]]))
+    print(' ')
+
+    # Test sample()
+    print('sample():')
+    print(gs.sample())
+    print(gs.sample(n_samples=3))
+    print(' ')
+
+    print('Internal data:')
+    print('weights:',gs.bgm.weights_)
+    print('means:',gs.bgm.means_)
+    print('covariances:',gs.bgm.covariances_)
+    print('precisions:',gs.bgm.precisions_)
+    print(' ')
 
     return
 
