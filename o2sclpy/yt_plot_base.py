4d235dc78d30a40e09cb2253bd49f7efd90a9b93
Andrew W. Steiner <awsteiner@mykolab.com>
2023-06-24 00:34:14 -0400

    Removing matplotlib from top of yt_plot_base.

--- o2sclpy/yt_plot_base.py
+++ o2sclpy/yt_plot_base.py
@@ -26,12 +26,6 @@ import os
 # For system type detection
 import platform
 
-# To create new color maps
-from matplotlib.colors import LinearSegmentedColormap
-
-# For rectangles and ellipses
-import matplotlib.patches as patches
-
 from o2sclpy.utils import parse_arguments, string_to_dict
 from o2sclpy.utils import force_bytes, default_plot, get_str_array
 from o2sclpy.utils import string_to_color
