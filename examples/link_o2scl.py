d2e2b3330dd7b7048d7c8b0dff7f4a571f4326e6
Andrew W. Steiner <awsteiner@mykolab.com>
2023-07-22 13:02:34 -0400

    Fixing more version numbers.

--- examples/link_o2scl.py
+++ examples/link_o2scl.py
@@ -32,5 +32,5 @@ link.link_o2scl()
 print(link.o2scl_settings.o2scl_version())
 
 def test_fun():
-    assert link.o2scl_settings.o2scl_version()==b'0.928'
+    assert link.o2scl_settings.o2scl_version()==b'0.929a1'
     return
