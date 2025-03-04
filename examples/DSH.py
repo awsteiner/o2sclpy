e97ff47245b9a0d429a44fb2a0475ca42c2b03c2
Andrew W. Steiner <awsteiner0@protonmail.com>
2025-03-04 17:09:11 -0500

    Updating dsh example.

--- examples/DSH.py
+++ examples/DSH.py
@@ -14,31 +14,30 @@ if 'pytest' in sys.modules:
     plots=False
 # -
 
-# Link the O$_2$scl library:
+# Get a copy (a pointer to) the O$_2$scl unit conversion object,
+# which also allows access to the constant library
 
-link=o2sclpy.linker()
-link.link_o2scl()
+o2scl_settings=o2sclpy.lib_settings_class()
+cu=o2scl_settings.get_convert_units()
 
-# Get a copy (a pointer to) the O$_2$scl unit conversion object, which
-# also allows access to the constant library, then get ħc.
+# Get ħc.
 
-cu=link.o2scl_settings.get_convert_units()
 ħc=cu.find_unique('hbarc','MeV*fm')
 print('ħc = %7.6e\n' % (ħc))
 
 # Use the cloud_file object to download the EOS
 
-cf=o2sclpy.cloud_file(link)
+cf=o2sclpy.cloud_file()
 cf.verbose=1
 cf.get_file('dsh.o2','https://isospin.roam.utk.edu/public_data'+
             '/eos_tables/du21/fid_3_5_22.o2')
 
 # Read the tensor which stores the average mass number
 
-hf=o2sclpy.hdf_file(link)
-tg_A=o2sclpy.tensor_grid(link)
+hf=o2sclpy.hdf_file()
+tg_A=o2sclpy.tensor_grid()
 hf.open('dsh.o2')
-o2sclpy.hdf_input_tensor_grid(link,hf,tg_A,'A')
+o2sclpy.hdf_input_tensor_grid(hf,tg_A,'A')
 hf.close()
 
 # In order to make a plot at fixed Ye, we first need to construct a
@@ -47,13 +46,13 @@ hf.close()
 # object), but for Ye, we select the value in the grid which is
 # closest to Ye=0.4.
 
-ix=o2sclpy.std_vector_size_t(link)
+ix=o2sclpy.std_vector_size_t()
 ix.resize(3)
 ix[1]=tg_A.lookup_grid(1,0.4)
 
 # Create a table3d object
 
-t3d=o2sclpy.table3d(link)
+t3d=o2sclpy.table3d()
 tg_A.copy_table3d_align_setxy(0,2,ix,t3d,'nB','T','A')
 
 # Now plot the results. Raw matplotlib works, but o2sclpy has
@@ -65,7 +64,7 @@ if plots:
     pl.xtitle(r'$ n_B~(\mathrm{fm}^{-3}) $')
     pl.ytitle(r'$ T~(\mathrm{MeV}) $')
     pl.ttext(1.25,0.5,u'$ A $',rotation=90)
-    pl.den_plot(t3d,'A')
+    pl.den_plot([t3d,'A'])
     plot.show()
 
 # For testing purposes
